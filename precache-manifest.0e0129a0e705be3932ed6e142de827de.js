dc71d06850c36f25eeadf7ea3aa0dc57f3fb3564
Ashim Saha <flashim@gmail.com>
2019-08-12 16:16:08 +0530

    -u

---
+++ precache-manifest.0e0129a0e705be3932ed6e142de827de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240bc0677c23ad101c1f7d751e9584ab",
+    "url": "/pokemon-viewer/index.html"
+  },
+  {
+    "revision": "1280ef17520c569ffffc",
+    "url": "/pokemon-viewer/static/css/main.d15229e1.chunk.css"
+  },
+  {
+    "revision": "26c0e2195792374044e0",
+    "url": "/pokemon-viewer/static/js/2.718a6d89.chunk.js"
+  },
+  {
+    "revision": "1280ef17520c569ffffc",
+    "url": "/pokemon-viewer/static/js/main.52d674df.chunk.js"
+  },
+  {
+    "revision": "4d1d5e5a1900e88a2be8",
+    "url": "/pokemon-viewer/static/js/runtime~main.1890450c.js"
+  }
+]);
